d244227ccc158386be9dd34f0c3df8664229a7e8
NadaffaSakho <nadaffasakho@gmail.com>
2023-11-23 20:54:42 +0700

    second commit

---
+++ forms/contact.js
@@ -0,0 +1,27 @@
+document.getElementById("whatsappForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    let contact = '6282225058290'; // add your number ex(+9100000000)
+
+    var encodedMessage = encodeURIComponent(
+        "Name: " + name + "\n" +
+        "Email: " + email + "\n" +
+        "Phone: " + subject + "\n" +
+        "Message: " + message
+    );
+
+    var link;
+
+    // Check if user is on a mobile device
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        link = `whatsapp://send?phone=${contact}&text=${encodedMessage}`;
+    } else { // Desktop device
+        link = `https://web.whatsapp.com/send?phone=${contact}&text=${encodedMessage}`;
+    }
+
+    window.open(link, '_blank');
+});
